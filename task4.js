222b39ad6d2f740cf6b9cc66778e091c9476e5c7
mattiagemmiti <mattiagemmiti92@gmail.com>
2023-11-19 13:24:51 +0100

    Comments added

--- task4.js
+++ task4.js
@@ -7,11 +7,11 @@
 
 // Function to apply Dijkstra's algorithm
 function dijkstraAlgorithm(adjMatrix, startNode) {
-    const numNodes = adjMatrix.length;
+    const numNodes = adjMatrix.length;                        // Get the number of nodes in the graph, which is equal to the length of the adjacency matrix
   
     // Initialize arrays to store distances and visited nodes
-    const distance = new Array(numNodes).fill(Infinity);
-    const visited = new Array(numNodes).fill(false);
+    const distance = new Array(numNodes).fill(Infinity);      // Initialize distance array with infinite values
+    const visited = new Array(numNodes).fill(false);          // Initialize visited array with false values
   
     // Distance from the start node to itself is 0
     distance[startNode] = 0;
@@ -37,18 +37,20 @@ function dijkstraAlgorithm(adjMatrix, startNode) {
   
     // Display the results in the console
     console.log("Shortest distances from node " + startNode + ":");
-    for (let i = 0; i < numNodes; i++) {
+    for (let i = 0; i < numNodes; i++) {                        //for loop to iterate over the sequence of distances from nodes
       console.log("To node " + i + ": " + distance[i]);
     }
   }
   
-  // Helper function to find the unvisited node with the minimum distance
-  function findMinDistanceNode(distance, visited) {
-    let minDistance = Infinity;
-    let minDistanceNode = -1;
-  
+  // Function to find the unvisited node with the minimum distance ( returns -1 when no unvisited node is found )
+  function findMinDistanceNode(distance, visited) {     // distance = array to store current shortest distances; //visited = array that keeps track of whether each node has been visited or not
+    let minDistance = Infinity;       //initially set to infinity to represent the largest distance
+    let minDistanceNode = -1;         //set to -1 to represent an invalid node index
+    
+    //The function loops through each node
     for (let i = 0; i < distance.length; i++) {
-      if (!visited[i] && distance[i] < minDistance) {
+    // If node has been visited and its distance to the set node is less than the minimum distance, it updates 'minDistance' and 'minDistanceNode' to the values of the current node
+      if (!visited[i] && distance[i] < minDistance) {   
         minDistance = distance[i];
         minDistanceNode = i;
       }
@@ -57,7 +59,7 @@ function dijkstraAlgorithm(adjMatrix, startNode) {
     return minDistanceNode;
   }
   
-  // Example usage with a prompt for simplicity
+  // Example of 9x9 adjancency matrix with pre-defined arrays of values
   const adjacencyMatrix = [
     [0, 4, 0, 0, 0, 0, 0, 8, 0],
     [4, 0, 8, 0, 0, 0, 0, 11, 0],
